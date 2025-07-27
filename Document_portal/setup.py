6e9a5bfc2b87cf13d4cc87e5971a263ac45e1cbe
Yaswanth V <yashwanth142004@gmail.com>
2025-07-27 11:25:39 +0530

    Project template created successfully

---
+++ Document_portal/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup , find_packages 
+
+setup(
+    name="document_portal",
+    author="Yaswanth Sai Valluru",
+    version="0.1",
+    packages=find_packages(),
+    # install_requires = [
+    #     "fastapi",
+    #     "uvicorn",
+    #     "python-dotenv",
+    # ],
+    
+    
+)
