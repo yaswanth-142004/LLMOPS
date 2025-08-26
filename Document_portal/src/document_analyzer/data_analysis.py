6b08f1ebe4c9c1734c2c5fe29b84c82ccd55e878
Yaswanth V <yashwanth142004@gmail.com>
2025-08-26 23:31:04 +0530

    Updated Ingestion code

--- Document_portal/src/document_analyzer/data_analysis.py
+++ Document_portal/src/document_analyzer/data_analysis.py
@@ -1,56 +1,58 @@
-from math import e
-import os 
+import os
 import sys
-from utils.model_loader import ModelLoader 
-from logger.custom_logger import CustomLogger 
-from exception.custom_exception import DocumentPortalException 
-from model.models import* 
-from langchain_core.output_parsers import JsonOutputParser 
-from langchain.output_parsers import OutputFixingParser 
-from prompt.prompt_library import *
+from utils.model_loader import ModelLoader
+from logger import GLOBAL_LOGGER as log
+from exception.custom_exception import DocumentPortalException
+from model.models import *
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+from prompt.prompt_library import PROMPT_REGISTRY # type: ignore
 
 class DocumentAnalyzer:
-    """Analyzes documents using a pre-trained model. 
-    
-    Automatically logs all actions and supports session-based organization.  
     """
-    
+    Analyzes documents using a pre-trained model.
+    Automatically logs all actions and supports session-based organization.
+    """
     def __init__(self):
-        self.log = CustomLogger().get_logger(__name__)
-        
         try:
-            self.loafer = ModelLoader()
-            self.llm = self.loader.load_llm()
+            self.loader=ModelLoader()
+            self.llm=self.loader.load_llm()
             
+            # Prepare parsers
             self.parser = JsonOutputParser(pydantic_object=Metadata)
-            self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser,llm=self.llm)
+            self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
             
-            self.prompt = prompt 
+            self.prompt = PROMPT_REGISTRY["document_analysis"]
             
-            self.log.info("Document Analyzer initialized successfully")
+            log.info("DocumentAnalyzer initialized successfully")
+            
             
         except Exception as e:
-            self.log.error(f"Error initializing DocumentAnalyzer :{e}")
-            raise DocumentPortalException("Error in DocumentAnalyzer initialization",sys)
+            log.error(f"Error initializing DocumentAnalyzer: {e}")
+            raise DocumentPortalException("Error in DocumentAnalyzer initialization", sys)
+        
+        
     
-    def analyze_document(self,document_text:str)->dict:
-        """Analyze a document's text and extract structured metadata and summary.
+    def analyze_document(self, document_text:str)-> dict:
         """
-        
+        Analyze a document's text and extract structured metadata & summary.
+        """
         try:
-            chain = self.prompt | self.llm | self.fixing_parser 
+            chain = self.prompt | self.llm | self.fixing_parser
             
-            self.log.info("Meta-data analysis chain initialized")
-            
+            log.info("Meta-data analysis chain initialized")
+
             response = chain.invoke({
-                "format_instructions":self.parser.get_format_instructions(),
-                "document_text":document_text
+                "format_instructions": self.parser.get_format_instructions(),
+                "document_text": document_text
             })
+
+            log.info("Metadata extraction successful", keys=list(response.keys()))
             
-            self.log.info("Metadata extraction successful ",keys=list(response.keys()))
-            
-            return response 
-        
+            return response
+
         except Exception as e:
-            self.log.error("Metadata analysis failed",error=str(e))
-            raise DocumentPortalException("Metadata extraction failed")from e
+            log.error("Metadata analysis failed", error=str(e))
+            raise DocumentPortalException("Metadata extraction failed",sys)
+        
+    
